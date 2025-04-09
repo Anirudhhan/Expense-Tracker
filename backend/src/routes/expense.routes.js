b67aec7dadcccc2fd034e11d47fcc362a962272b
Anirudhhan <anirudhhan.ashok@gmail.com>
2025-04-09 19:12:07 +0530

    updates

--- backend/src/routes/expense.routes.js
+++ backend/src/routes/expense.routes.js
@@ -4,8 +4,8 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/add-expense", protectRoute, addEntry);
-router.get("/entries/:type", protectRoute, fetchAllEntries);
+router.post("/add-entry", protectRoute, addEntry);
+router.get("/fetch-entries/:type", protectRoute, fetchAllEntries);
 
 router.delete("/delete/:id", protectRoute, deleteEntry);
 
