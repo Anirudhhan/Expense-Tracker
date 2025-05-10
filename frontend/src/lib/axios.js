26eeb7f2053761950819447fef5a861d15a28a3f
Anirudhhan Ashok <139037978+Anirudhhan@users.noreply.github.com>
2025-05-10 20:44:44 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: `${process.env.REACT_APP_API}/api`,
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
     withCredentials: true,
 })
